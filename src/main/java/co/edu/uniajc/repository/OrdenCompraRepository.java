85d51f40e1502df7a57a3dda86f45653943e8fef
oaguilar01 <oeaguilar4@gmail.com>
2025-04-01 23:19:58 -0500

    Ajuste Clase OrdenCompraModel

--- src/main/java/co/edu/uniajc/repository/OrdenCompraRepository.java
+++ src/main/java/co/edu/uniajc/repository/OrdenCompraRepository.java
@@ -1,5 +1,6 @@
 package co.edu.uniajc.repository;
 
+import co.edu.uniajc.model.OrdenCompraModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
